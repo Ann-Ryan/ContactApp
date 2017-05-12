f40589b8b09b40b766056f6af64bebad13efb7fe
AnnikinSkywalker <ann35173@gmail.com>
2017-05-12 09:23:02 -0700

    Updated database helper and main activity to create a database
    Added name field and "add" button
    
    I am Ann Ryan.

--- GitHub.appref-ms/app/src/main/java/com/example/ryana7853/contactapp/MainActivity.java
+++ GitHub.appref-ms/app/src/main/java/com/example/ryana7853/contactapp/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.ryana7853.contactapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+
+    DatabaseHelper myDb;
+    EditText editName;
+    Button btnAddData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
     }
 
     @Override
