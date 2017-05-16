9aa28eab5c1284acb28fd8bb8809910ff673f14b
AnnikinSkywalker <ann35173@gmail.com>
2017-05-16 09:30:47 -0700

    Today the cursor was added and different toasts
    The app closes on the phone every time I press 'add data,' and errors
    are encountered in the add data method when the number and email are put
    into the data table.

--- GitHub.appref-ms/app/src/main/java/com/example/ryana7853/contactapp/DatabaseHelper.java
+++ GitHub.appref-ms/app/src/main/java/com/example/ryana7853/contactapp/DatabaseHelper.java
@@ -1,8 +1,11 @@
 package com.example.ryana7853.contactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by ryana7853 on 5/11/2017.
@@ -14,6 +17,8 @@ public class DatabaseHelper extends SQLiteOpenHelper
     public static final String TABLE_NAME = "Contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+    public static final String COL_3 = "NUMBER";
+    public static final String COL_4 = "EMAIL";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -30,4 +35,61 @@ public class DatabaseHelper extends SQLiteOpenHelper
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
+
+    public boolean insertData(String name, String number, String email){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+
+        Log.d("MyContact", String.valueOf(contentValues.valueSet()));
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        Log.d("MyContact", String.valueOf(result));
+        if(result == -1) return false;
+
+        contentValues.put(COL_3, number);
+
+        Log.d("MyContact", String.valueOf(contentValues.valueSet()));
+        result = db.insert(TABLE_NAME, null, contentValues);
+        Log.d("MyContact", String.valueOf(result));
+        if(result == -1) return false;
+
+        contentValues.put(COL_4, email);
+
+        Log.d("MyContact", String.valueOf(contentValues.valueSet()));
+        result = db.insert(TABLE_NAME, null, contentValues);
+        Log.d("MyContact", String.valueOf(result));
+        if(result == -1) return false;
+
+
+        /*
+        contentValues.put(COL_2, name);
+        Log.d("MyContact", String.valueOf(contentValues.valueSet()));
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        Log.d("MyContact", String.valueOf(result));
+        if(result == -1) return false;
+        contentValues.put(COL_3, number);
+        Log.d("MyContact", String.valueOf(contentValues.valueSet()));
+        long result2 = db.insert(TABLE_NAME, null, contentValues);
+        Log.d("MyContact", String.valueOf(result2));
+        //this is the part that returns negative one and fails
+         if(result2 == -1) return false;
+        contentValues.put(COL_4, email);
+        Log.d("MyContact", String.valueOf(contentValues.valueSet()));
+        long result3 = db.insert(TABLE_NAME, null, contentValues);
+        Log.d("MyContact", String.valueOf(result3));
+        if(result3 == -1) return false;
+        */
+
+        return true;
+
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+
+
+
 }
--- GitHub.appref-ms/app/src/main/java/com/example/ryana7853/contactapp/MainActivity.java
+++ GitHub.appref-ms/app/src/main/java/com/example/ryana7853/contactapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.ryana7853.contactapp;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editNumber;
+    EditText editEmail;
     Button btnAddData;
 
 
@@ -23,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+        editName = (EditText) findViewById(R.id.editText_name);
+        editNumber = (EditText) findViewById(R.id.editText_number);
+        editEmail = (EditText) findViewById(R.id.editText_email);
     }
 
     @Override
@@ -45,5 +55,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addData (View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editNumber.getText().toString(), editEmail.getText().toString());
+        if(isInserted){
+            //create toast method to user indicating data inserted correctly
+            Log.d("MyContact", "Data insertion successful!");
+            String message = "SUCCESS!";
+            int duration  = Toast.LENGTH_SHORT;
+            Context context = getApplicationContext();
+           // Toast toast = new Toast(this, message, duration);
+           Toast t = new Toast(context);
+            t.setDuration(duration);
+            t.setText(message);
+            t.show();
+        }
+        else{
+            //create toast method to user indicating data inserted incorrectly!
+            Log.d("MyContact", "Data insertion unsuccessful!");
+            String message = "FAILURE!";
+            int duration  = Toast.LENGTH_SHORT;
+            Context context = getApplicationContext();
+            Toast t = new Toast(context);
+            t.setText(message);
+            t.setDuration(duration);
+            t.show();
+        }
+    }
+
+    public void viewData(View v){
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found in database");
+            //put a log.d message and toast
+            Log.d("MyContact", "No data found in database!");
+        }
+        StringBuffer buffer = new StringBuffer();
+        //set up while loop with curser moveToNext method
+        //append each col to buffer
+        //use getString method
+        showMessage("Data", buffer.toString());
+    }
+
+    public void showMessage(String title, String message){
+
     }
 }
